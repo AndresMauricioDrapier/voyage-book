40236db4327ac8048c1ef1c73c03c8c1a965612f
Satiring <escuelaps@gmail.com>
2024-03-13 18:22:25 +0100

    Remove Card Import in Resizable Container Component

--- src/components/dashboard/resizable-container.tsx
+++ src/components/dashboard/resizable-container.tsx
@@ -4,9 +4,6 @@ import {
     ResizablePanel,
     ResizablePanelGroup,
 } from "@/components/ui/resizable"
-import {
-    Card,
-  } from "@/components/ui/card"
 
 type ResizableContent = {
     content: React.ReactNode,
